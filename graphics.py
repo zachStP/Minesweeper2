5477fbb049b3d77b6fac43d99c0f0f69cce3418e
kittkat7 <rainbowtabby7@gmail.com>
2023-04-01 22:13:58 -0400

    Added reading board data

--- graphics.py
+++ graphics.py
@@ -51,10 +51,23 @@ def update_scale(sqrgrid):
     t_padding = tile_size / 10
 
 
-def draw_board(board: list[list[int]], player):
+def draw_board(board_dat: list[list[int]], board_vis: list[list[bool]], player):
 
     # update scales
     update_scale(4)
+    
+    board = []
+
+    for row in range(len(board_vis)):
+        r = []
+        for col in range(len(board_vis[row])):
+            if board_vis[row][col]:
+                r.append(board_dat[row][col])
+            else:
+                r.append(99)
+        board.append(r)
+        # end for col
+    # end for row
 
     for row in range(len(board)):
         for col in range(len(board[row])):
@@ -62,12 +75,29 @@ def draw_board(board: list[list[int]], player):
             posy = row * tile_size + b_pad_y
             # draw border
             pg.draw.rect(display, (50, 50, 50), (posx-1, posy-1, tile_size+1, tile_size+1))
-            # draw tile background
-            pg.draw.rect(display, (255, 255, 255), (posx + t_padding / 2, posy + t_padding / 2,
-                tile_size - t_padding, tile_size - t_padding))
+            
+            # draw tile 
+            # 99 = not clear
+            if board[row][col] == 99:
+                pg.draw.rect(display, (255, 255, 255), (posx + t_padding / 2, posy + t_padding / 2,
+                    tile_size - t_padding, tile_size - t_padding))
+            # 0 = clear
+            elif board[row][col] == 0:
+                pg.draw.rect(display, (100, 100, 100), (posx + t_padding / 2, posy + t_padding / 2,
+                    tile_size - t_padding, tile_size - t_padding))
+            # 9 = mine
+            elif board[row][col] == 9:
+                pg.draw.rect(display, (255, 0, 0), (posx + t_padding / 2, posy + t_padding / 2,
+                    tile_size - t_padding, tile_size - t_padding))
+            # 1-8 = clear
+            else:
+                pg.draw.rect(display, (0, 0, board[row][col] * 10), (posx + t_padding / 2, posy + t_padding / 2,
+                    tile_size - t_padding, tile_size - t_padding))
+        
         # end for col
     # end for row
+    
+    # display player
     display.blit(pg.transform.smoothscale(player_ico.convert_alpha(),
         (tile_size - t_padding, tile_size - t_padding,)),
-        (1 * tile_size + t_padding / 2 + b_pad_x, 1 * tile_size + t_padding / 2 + b_pad_y))
-    print(4 * tile_size + t_padding / 2 + b_pad_x)
+        (player.x * tile_size + t_padding / 2 + b_pad_x, player.y * tile_size + t_padding / 2 + b_pad_y))
