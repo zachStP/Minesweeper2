3758b9bd11714dfbdd7e704a45feaacd9eca9a13
BenOwnby <ownbyb@appstate.edu>
2023-04-01 21:51:29 -0400

    added player class

---
+++ Player.py
@@ -0,0 +1,24 @@
+class Player:
+    def __init__(self, x:int, y:int):
+        self.x = x
+        self.y = y
+        self.isDead = False
+
+    def moveUp(self):
+        if self.y - 1 >= 0:
+            self.y -= 1
+
+    def moveDown(self):
+        if self.y + 1 < 16:
+            self.y += 1
+
+    def moveLeft(self):
+        if self.x - 1 >= 0:
+            self.x -= 1
+
+    def moveRight(self):
+        if self.x + 1 < 16:
+            self.x += 1
+
+    def kill(self):
+        self.isDead = True
