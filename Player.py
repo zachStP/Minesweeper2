253e288c255853fddfeb254f6b45cf01259aa9e7
zach <zachary.stpierre2022@gmail.com>
2023-04-02 12:33:10 -0400

    finished with flag Func

--- Player.py
+++ Player.py
@@ -40,7 +40,8 @@ class Player:
         cellsAtDirWithDis2 = [(self.y - 2, self.x), (self.y + 2, self.x),
                           (self.y, self.x + 2), (self.y, self.x - 2)]
         if dir == "up":
-            #cellWithDis1 = cellsAtDirWithDis1[0]
+            cellWithDis1 = cellsAtDirWithDis1[0]
+            cellWithDis2 = cellsAtDirWithDis2[1]
 
             if(board.isMine(board.data, self.y - 1, self.x)):
                 if(board.isMine(board.data, self.y - 2, self.x)):
@@ -48,7 +49,7 @@ class Player:
                     board.changeDataAtTo(self.y - 2, self.x, 10)
                 elif(board.isValid(board.data, self.y - 2, self.x)):
                     board.moveMineAtTo(self.y - 1, self.x, self.y - 2, self.x)
-                    
+            
         if dir == "down":
             if(board.isMine(board.data, self.y + 1, self.x)):
                 if(board.isMine(board.data, self.y + 2, self.x)):
@@ -72,17 +73,24 @@ class Player:
                 elif(board.isValid(board.data, self.y, self.x - 2)):
                     board.moveMineAtTo(self.y, self.x - 1, self.y, self.x - 2)
 
+       """ if(board.isMine(board.data, cellWithDis1[0], cellWithDis1[1])):
+            if(board.isMine(board.data, cellWithDis2[0], cellWithDis2[1])):
+                board.changeDataAtTo(cellWithDis1[0], cellWithDis1[1], 0)
+                board.changeDataAtTo(cellWithDis2[0], cellWithDis2[1], 10)
+            elif(board.isValid(board.data, cellWithDis2[0], cellWithDis2[1])):
+                board.moveMineAtTo(cellWithDis1[0], cellWithDis1[1], cellWithDis2[0], cellWithDis2[1])"""
+
     def flag(self, board, dir):
         if dir == "up":
             if(board.isValid(board.data, self.y - 1, self.x)):
-                board.changeDataAtTo(self.y - 1, self.x, -1 * board.data)   
+                board.changeDataAtTo(self.y - 1, self.x, -1 * board.data[self.y - 1][self.x])   
         if dir == "down":
             if(board.isValid(board.data, self.y + 1, self.x)):
-                board.changeDataAtTo(self.y + 1, self.x,  -1 * board.data)
+                board.changeDataAtTo(self.y + 1, self.x,  -1 * board.data[self.y + 1][self.x])
         if dir == "right":
             if(board.isValid(board.data, self.y, self.x + 1)):
-                board.changeDataAtTo(self.y, self.x + 1,  -1 * board.data)
+                board.changeDataAtTo(self.y, self.x + 1,  -1 * board.data[self.y][self.x + 1])
         if dir == "left":
             if(board.isValid(board.data, self.y, self.x - 1)):
-                board.changeDataAtTo(self.y, self.x - 1,  -1 * board.data)
+                board.changeDataAtTo(self.y, self.x - 1,  -1 * board.data[self.y][self.x - 1])
                 
